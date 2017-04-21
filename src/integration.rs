c9ce4601b9b362ffe6d2270fcfb0fecf62e15965
Casey Rodarmor <casey@rodarmor.com>
2017-04-20 23:06:03 -0700

    Add --shell option to specify the shell to use (#175)
    Mostly for debugging purposes, so I can make sure behavior is
    consistent across different shells. Although I suppose this might
    also be of use if you've got a mega weird `sh` that you'd like to
    avoid.
    
    Defaults to `sh` so current behavior is preserved.

--- src/integration.rs
+++ src/integration.rs
@@ -938,7 +938,7 @@ fn quiet_flag_or_dry_run_flag() {
     "error: The argument '--dry-run' cannot be used with '--quiet'
 
 USAGE:
-    just --color <color> --quiet
+    just --color <color> --quiet --shell <shell>
 
 For more information try --help\n",
   );
