e57b636313001d4e9a02678299906012243951f6
Monami Sharma <monami.sharma@gmail.com>
2018-09-12 21:46:55 -0500

    Allow `--justfile` without `--working-directory`

---
+++ tests/working_directory.rs
@@ -0,0 +1,35 @@
+extern crate executable_path;
+extern crate tempdir;
+
+use executable_path::executable_path;
+use std::{error::Error, fs, process::Command};
+use tempdir::TempDir;
+
+/// Test that just runs with the correct working directory when invoked with
+/// `--justfile` but not `--working-directory`
+#[test]
+fn justfile_without_working_directory() -> Result<(), Box<Error>> {
+  let tmp = TempDir::new("just-integration")?;
+  let justfile = tmp.path().join("justfile");
+  let data = tmp.path().join("data");
+  fs::write(
+    &justfile,
+    "foo = `cat data`\ndefault:\n echo {{foo}}\n cat data",
+  )?;
+  fs::write(&data, "found it")?;
+
+  let output = Command::new(executable_path("just"))
+    .arg("--justfile")
+    .arg(&justfile)
+    .output()?;
+
+  if !output.status.success() {
+    panic!()
+  }
+
+  let stdout = String::from_utf8(output.stdout).unwrap();
+
+  assert_eq!(stdout, "found it\nfound it");
+
+  Ok(())
+}
