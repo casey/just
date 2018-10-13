d3e33ab9525c7842354deceb14e3a2b1b55ece40
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-12 22:04:01 -0700

    Move all code into library crate

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate log;
+
+extern crate ansi_term;
+extern crate brev;
+extern crate clap;
+extern crate ctrlc;
+extern crate dotenv;
+extern crate edit_distance;
+extern crate env_logger;
+extern crate itertools;
+extern crate libc;
+extern crate regex;
+extern crate target;
+extern crate tempdir;
+extern crate unicode_width;
+
+#[cfg(test)]
+#[macro_use]
+mod testing;
+
+#[macro_use]
+mod die;
+
+mod assignment_evaluator;
+mod assignment_resolver;
+mod color;
+mod command_ext;
+mod common;
+mod compilation_error;
+mod configuration;
+mod cooked_string;
+mod expression;
+mod fragment;
+mod function;
+mod interrupt_handler;
+mod justfile;
+mod lexer;
+mod load_dotenv;
+mod misc;
+mod parameter;
+mod parser;
+mod platform;
+mod range_ext;
+mod recipe;
+mod recipe_resolver;
+mod run;
+mod runtime_error;
+mod shebang;
+mod token;
+mod verbosity;
+
+use common::*;
+
+pub use run::run;
+pub use lexer::Lexer;
+pub use parser::Parser;
+pub use justfile::Justfile;
+pub use recipe::Recipe;
+pub use compilation_error::{CompilationError, CompilationErrorKind};
