0c2f30b1e802b3d52b21e30ec8d485ce76ac3534
Zhenhui Xie <xiezh0831@yahoo.co.jp>
2020-03-13 15:25:43 +0800

    Update zsh completion file
    I just modified the clap-generated completion file for `zsh` to make it working.
    
    Note that completion for `--set` calls `just --evaluate` to get the variables, so it can slow down the console a bit if you have heavy evaluation logic.

--- completions/just.zsh
+++ completions/just.zsh
@@ -5,7 +5,7 @@ autoload -U is-at-least
 _just() {
     typeset -A opt_args
     typeset -a _arguments_options
-    local ret=1
+    local context curcontext="$curcontext" state line ret=1
 
     if is-at-least 5.2; then
         _arguments_options=(-s -S -C)
@@ -13,49 +13,77 @@ _just() {
         _arguments_options=(-s -C)
     fi
 
-    local context curcontext="$curcontext" state line
-    _arguments "${_arguments_options[@]}" \
-'--color=[Print colorful output]: :(auto always never)' \
-'-f+[Use <JUSTFILE> as justfile.]' \
-'--justfile=[Use <JUSTFILE> as justfile.]' \
-'*--set=[Override <VARIABLE> with <VALUE>]' \
-'--shell=[Invoke <SHELL> to run recipes]' \
-'*--shell-arg=[Invoke shell with <SHELL-ARG> as an argument]' \
-'-d+[Use <WORKING-DIRECTORY> as working directory. --justfile must also be set]' \
-'--working-directory=[Use <WORKING-DIRECTORY> as working directory. --justfile must also be set]' \
-'--completions=[Print shell completion script for <SHELL>]: :(zsh bash fish powershell elvish)' \
-'-s+[Show information about <RECIPE>]' \
-'--show=[Show information about <RECIPE>]' \
-'(-q --quiet)--dry-run[Print what just would do without doing it]' \
-'--highlight[Highlight echoed recipe lines in bold]' \
-'--no-highlight[Don'\''t highlight echoed recipe lines in bold]' \
-'(--dry-run)-q[Suppress all output]' \
-'(--dry-run)--quiet[Suppress all output]' \
-'--clear-shell-args[Clear shell arguments]' \
-'*-v[Use verbose output]' \
-'*--verbose[Use verbose output]' \
-'--dump[Print entire justfile]' \
-'-e[Edit justfile with editor given by $VISUAL or $EDITOR, falling back to `vim`]' \
-'--edit[Edit justfile with editor given by $VISUAL or $EDITOR, falling back to `vim`]' \
-'--evaluate[Print evaluated variables]' \
-'--init[Initialize new justfile in project root]' \
-'-l[List available recipes and their arguments]' \
-'--list[List available recipes and their arguments]' \
-'--summary[List names of available recipes]' \
-'-h[Print help information]' \
-'--help[Print help information]' \
-'-V[Print version information]' \
-'--version[Print version information]' \
-'::ARGUMENTS -- Overrides and recipe(s) to run, defaulting to the first recipe in the justfile:_files' \
-&& ret=0
-    
+    local common=(
+        '--color=[Print colorful output]: :(auto always never)' 
+        '(-f --justfile)'{-f+,--justfile=}'[Use <JUSTFILE> as justfile.]' 
+        '*--set[Override <VARIABLE> with <VALUE>]: :_just_variables' 
+        '--shell=[Invoke <SHELL> to run recipes]' 
+        '*--shell-arg=[Invoke shell with <SHELL-ARG> as an argument]' 
+        '(-d --working-directory)'{-d+,--working-directory}'[Use <WORKING-DIRECTORY> as working directory. --justfile must also be set]' 
+        '--completions=[Print shell completion script for <SHELL>]: :(zsh bash fish powershell elvish)' 
+        '(-s --show)'{-s+,--show=}'[Show information about <RECIPE>]: :_just_commands' 
+        '(-q --quiet)--dry-run[Print what just would do without doing it]' 
+        '(--dry-run)'{-q,--quiet}'[Suppress all output]' 
+        '(--no-highlight)--highlight[Highlight echoed recipe lines in bold]' 
+        '(--highlight)--no-highlight[Don'\''t highlight echoed recipe lines in bold]' 
+        '--clear-shell-args[Clear shell arguments]' 
+        '*'{-v,--verbose}'[Use verbose output]' 
+        '--dump[Print entire justfile]' 
+        '(- 1 *)'{-e,--edit}'[Edit justfile with editor given by $VISUAL or $EDITOR, falling back to `vim`]' 
+        '--evaluate[Print evaluated variables]' 
+        '--init[Initialize new justfile in project root]' 
+        '(- 1 *)'{-l,--list}'[List available recipes and their arguments]' 
+        '(- 1 *)--summary[List names of available recipes]' 
+        '(- 1 *)'{-h,--help}'[Print help information]' 
+        '(- 1 *)'{-V,--version}'[Print version information]' 
+    )
+
+    _arguments "${_arguments_options[@]}" $common \
+        '1: :_just_commands' \
+        '*: :->args' \
+        && ret=0
+
+    case $state in
+        args)
+            curcontext="${curcontext%:*}-${words[2]}:"
+
+            # Display usage
+            local -a args_str args
+            args_str="`just --show ${words[2]}`"
+
+            echo $args_str >> /tmp/debug
+
+            lastarg=${words[${#words}]}
+
+            if [[ ${lastarg} = */* ]]; then
+                # Arguments contain slash would be recognised as a file
+                _arguments -s -S $common '*:: :_files'
+            else
+                # Show usage message
+                _message "%BUsage%b $args_str"
+                # Or complete with other commands
+                #_arguments -s -S $common '*:: :_just_commands'
+            fi
+
+        ;;
+    esac
+
+    return ret
+}
+
+(( $+functions[_just_variables] )) ||
+_just_variables() {
+    local -a variables
+
+    variables=( ${"${(f)$(_call_program commands just --evaluate)}"/ ##:= ##/:} )
+    _describe -t variables 'variables' variables
 }
 
 (( $+functions[_just_commands] )) ||
 _just_commands() {
-    local commands; commands=(
-        
-    )
+    local -a commands
+
+    commands=( ${${${(M)"${(f)$(_call_program commands just --list)}":#    *}/ ##/}/ ##/:Args: } )
     _describe -t commands 'just commands' commands "$@"
 }
 
