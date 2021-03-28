d81d17ba0a2f2e4a901e215a569eae3100ecbab5
Casey Rodarmor <casey@rodarmor.com>
2021-03-28 16:02:56 -0700

    Release v0.8.6 (#776)
    - Bump version: 0.8.5 → 0.8.6
    - Update dependencies
    - Update changelog
    - Update man page
    - Update config test

--- src/config.rs
+++ src/config.rs
@@ -860,7 +860,7 @@ mod tests {
   // have proper tests for all the flags, but this will do for now.
   #[test]
   fn help() {
-    const EXPECTED_HELP: &str = "just v0.8.5
+    const EXPECTED_HELP: &str = "just v0.8.6
 Casey Rodarmor <casey@rodarmor.com>
 🤖 Just a command runner \
                                  - https://github.com/casey/just
