1486a9d23ff8a1ccf3f1a000ec5f7e567d8d45f3
Casey Rodarmor <casey@rodarmor.com>
2021-04-05 21:09:46 -0700

    Allow evaluating an empty justfile

--- tests/misc.rs
+++ tests/misc.rs
@@ -570,25 +570,6 @@ echo `echo command interpolation`
 }
 
 test! {
-  name:     evaluate,
-  justfile: r#"
-foo := "a\t"
-hello := "c"
-bar := "b\t"
-ab := foo + bar + hello
-
-wut:
-  touch /this/is/not/a/file
-"#,
-  args:     ("--evaluate"),
-  stdout:   r#"ab    := "a	b	c"
-bar   := "b	"
-foo   := "a	"
-hello := "c"
-"#,
-}
-
-test! {
   name:     line_error_spacing,
   justfile: r#"
 
