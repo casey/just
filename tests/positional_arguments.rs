2abdeb386e5bd31a9516616a6cae8af4ecbd512c
Casey Rodarmor <casey@rodarmor.com>
2021-05-02 03:25:43 -0700

    Pass evaluated arguments as positional arguments (#810)

--- tests/positional_arguments.rs
+++ tests/positional_arguments.rs
@@ -64,3 +64,43 @@ test! {
   args:   ("foo", "a", "b", "c"),
   stdout: "a\na b c\n",
 }
+
+test! {
+  name: default_arguments,
+  justfile: r#"
+    set positional-arguments
+
+    foo bar='baz':
+      echo $1
+  "#,
+  args:   (),
+  stdout: "baz\n",
+  stderr: "echo $1\n",
+}
+
+test! {
+  name: empty_variadic_is_undefined,
+  justfile: r#"
+    set positional-arguments
+
+    foo *bar:
+      if [ -n "${1+1}" ]; then echo defined; else echo undefined; fi
+  "#,
+  args:   (),
+  stdout: "undefined\n",
+  stderr: "if [ -n \"${1+1}\" ]; then echo defined; else echo undefined; fi\n",
+}
+
+test! {
+  name: variadic_arguments_are_separate,
+  justfile: r#"
+    set positional-arguments
+
+    foo *bar:
+      echo $1
+      echo $2
+  "#,
+  args:   ("foo", "a", "b"),
+  stdout: "a\nb\n",
+  stderr: "echo $1\necho $2\n",
+}
