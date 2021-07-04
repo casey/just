25e1fc2f420ac36285256307d4b537cb48cb3794
Casey Rodarmor <casey@rodarmor.com>
2021-07-03 21:08:45 -0700

    Fix tests

--- bin/ref-type/tests/integration.rs
+++ bin/ref-type/tests/integration.rs
@@ -2,7 +2,7 @@ use executable_path::executable_path;
 use std::{process::Command, str};
 
 fn stdout(reference: &str) -> String {
-  let output = Command::new(executable_path("prerelease"))
+  let output = Command::new(executable_path("ref-type"))
     .args(&["--reference", reference])
     .output()
     .unwrap();
@@ -13,30 +13,30 @@ fn stdout(reference: &str) -> String {
 }
 
 #[test]
-fn junk_is_prerelease() {
-  assert_eq!(stdout("refs/tags/asdf"), "::set-output name=value::true\n");
+fn junk_is_other() {
+  assert_eq!(stdout("refs/tags/asdf"), "::set-output name=value::other\n");
 }
 
 #[test]
-fn valid_version_is_not_prerelease() {
+fn valid_version_is_release() {
   assert_eq!(
     stdout("refs/tags/0.0.0"),
-    "::set-output name=value::false\n"
+    "::set-output name=value::release\n"
   );
 }
 
 #[test]
-fn valid_version_with_trailing_characters_is_prerelease() {
+fn valid_version_with_trailing_characters_is_other() {
   assert_eq!(
     stdout("refs/tags/0.0.0-rc1"),
-    "::set-output name=value::true\n"
+    "::set-output name=value::other\n"
   );
 }
 
 #[test]
-fn valid_version_with_lots_of_digits_is_not_prerelease() {
+fn valid_version_with_lots_of_digits_is_release() {
   assert_eq!(
     stdout("refs/tags/01232132.098327498374.43268473849734"),
-    "::set-output name=value::false\n"
+    "::set-output name=value::release\n"
   );
 }
