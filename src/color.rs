542186eb41b86cb27f3f373ca918d32e4a18e324
Casey Rodarmor <casey@rodarmor.com>
2021-07-19 18:03:07 -0700

    Placate clippy

--- src/color.rs
+++ src/color.rs
@@ -41,21 +41,21 @@ impl Color {
   pub(crate) fn auto() -> Self {
     Self {
       use_color: UseColor::Auto,
-      ..Default::default()
+      ..Color::default()
     }
   }
 
   pub(crate) fn always() -> Self {
     Self {
       use_color: UseColor::Always,
-      ..Default::default()
+      ..Color::default()
     }
   }
 
   pub(crate) fn never() -> Self {
     Self {
       use_color: UseColor::Never,
-      ..Default::default()
+      ..Color::default()
     }
   }
 
