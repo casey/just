fc9dee2f743df7af50d03df7585f61349c53b018
Casey Rodarmor <casey@rodarmor.com>
2021-07-26 01:17:18 -0700

    rename loader arena

--- src/loader.rs
+++ src/loader.rs
@@ -1,12 +1,14 @@
 use crate::common::*;
 
 pub(crate) struct Loader {
-  src: Arena<String>,
+  arena: Arena<String>,
 }
 
 impl Loader {
   pub(crate) fn new() -> Self {
-    Loader { src: Arena::new() }
+    Loader {
+      arena: Arena::new(),
+    }
   }
 
   pub(crate) fn load<'src>(&'src self, path: &Path) -> RunResult<&'src str> {
@@ -14,6 +16,6 @@ impl Loader {
       path: path.to_owned(),
       io_error,
     })?;
-    Ok(self.src.alloc(src))
+    Ok(self.arena.alloc(src))
   }
 }
