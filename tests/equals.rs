39301e9f8b44ac8d1a024c5619eb5a302d47ff97
Casey Rodarmor <casey@rodarmor.com>
2021-10-01 18:37:28 -0700

    Remove deprecated equals error (#985)

---
+++ tests/equals.rs
@@ -0,0 +1,29 @@
+use crate::common::*;
+
+#[test]
+fn export_recipe() {
+  Test::new()
+    .justfile(
+      "
+      export foo='bar':
+        echo {{foo}}
+    ",
+    )
+    .stdout("bar\n")
+    .stderr("echo bar\n")
+    .run();
+}
+
+#[test]
+fn alias_recipe() {
+  Test::new()
+    .justfile(
+      "
+      alias foo='bar':
+        echo {{foo}}
+    ",
+    )
+    .stdout("bar\n")
+    .stderr("echo bar\n")
+    .run();
+}
