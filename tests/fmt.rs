c0596216f591719ba29c932943339e8ee7c7384b
hdhoang <code@hdhoang.space>
2021-10-14 17:27:06 +0700

    fmt: check formatting with `--check`

--- tests/fmt.rs
+++ tests/fmt.rs
@@ -11,6 +11,51 @@ test! {
   status: EXIT_FAILURE,
 }
 
+test! {
+  name: check_without_fmt,
+  justfile: "",
+  args: ("--check"),
+  stderr_regex: "error: The following required arguments were not provided:
+    --fmt
+(.|\\n)+",
+  status: EXIT_FAILURE,
+}
+
+test! {
+  name: dry_run_ok,
+  justfile: r#"
+# comment   with   spaces
+
+export x := `backtick
+with
+lines`
+
+recipe: deps
+    echo "$x"
+
+deps:
+    echo {{ x }}
+    echo '$x'
+"#,
+  args: ("--unstable", "--fmt", "--check"),
+  status: EXIT_SUCCESS,
+}
+
+test! {
+  name: dry_run_found_diff,
+  justfile: "x:=``\n",
+  args: ("--unstable", "--fmt", "--check"),
+  stdout: "
+  @@ -1 +1 @@
+  -x:=``
+  +x := ``
+  ",
+  stderr: "
+    error: Formatted justfile differs from original.
+  ",
+  status: EXIT_FAILURE,
+}
+
 #[test]
 fn unstable_passed() {
   let tmp = tempdir();
