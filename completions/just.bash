c7bd1d1c1ab9aec172381abc5c5a6578bc9dc18f
JP Bochi <jpbochi@pm.me>
2022-08-04 16:58:52 +0200

    allow bash completion to complete tasks in other directories

--- completions/just.bash
+++ completions/just.bash
@@ -25,7 +25,15 @@ _just() {
                     COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                     return 0
                 elif [[ ${COMP_CWORD} -eq 1 ]]; then
-                    local recipes=$(just --summary --color never 2> /dev/null)
+                    if echo "${cur}" | grep -qF '/'; then
+                        local path_prefix=$(echo "${cur}" | sed 's/[/][^/]*$/\//')
+                        # TODO: Replace tr and awk below with --list-prefix "${path_prefix}". Need to update the --summary option to respect that first.
+                        local recipes=$(just --summary --color never 2> /dev/null -- "${path_prefix}" \
+                                | tr '[:blank:]' '\n' | awk "{print \"${path_prefix}\"\$0}")
+                    else
+                        local recipes=$(just --summary --color never 2> /dev/null)
+                    fi
+
                     if [[ $? -eq 0 ]]; then
                         COMPREPLY=( $(compgen -W "${recipes}" -- "${cur}") )
                         return 0
