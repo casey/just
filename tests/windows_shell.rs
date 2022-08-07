b1c0e8df20257f250bae22fe569cfe8628294ac6
Casey Rodarmor <casey@rodarmor.com>
2022-08-07 16:45:01 -0700

    Add test

--- tests/windows_shell.rs
+++ tests/windows_shell.rs
@@ -6,6 +6,25 @@ fn windows_shell_setting() {
     .justfile(
       r#"
       set windows-shell := ["pwsh.exe", "-NoLogo", "-Command"]
+      set shell := ["asdfasdfasdfasdf"]
+
+      foo:
+        Write-Output bar
+    "#,
+    )
+    .shell(false)
+    .stdout("bar\r\n")
+    .stderr("Write-Output bar\n")
+    .run();
+}
+
+#[test]
+fn windows_powershell_setting_uses_powershell() {
+  Test::new()
+    .justfile(
+      r#"
+      set windows-powershell
+      set shell := ["asdfasdfasdfasdf"]
 
       foo:
         Write-Output bar
