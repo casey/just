205c519df927381490d79ac68c231d79e5a9dab3
JP Bochi <jpbochi@users.noreply.github.com>
2022-08-10 10:30:11 +0200

    remove color option, as it is not needed
    Co-authored-by: Casey Rodarmor <casey@rodarmor.com>

--- src/completions.rs
+++ src/completions.rs
@@ -166,11 +166,11 @@ pub(crate) const BASH_COMPLETION_REPLACEMENTS: &[(&str, &str)] = &[
                     COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                     return 0
                 elif [[ ${COMP_CWORD} -eq 1 ]]; then
-                    local recipes=$(just --summary --color never 2> /dev/null)
+                    local recipes=$(just --summary 2> /dev/null)
 
                     if echo "${cur}" | grep -qF '/'; then
                         local path_prefix=$(echo "${cur}" | sed 's/[/][^/]*$/\//')
-                        local recipes=$(just --summary --color never 2> /dev/null -- "${path_prefix}")
+                        local recipes=$(just --summary 2> /dev/null -- "${path_prefix}")
                         local recipes=$(printf "${path_prefix}%s" $recipes)
                     fi
 
