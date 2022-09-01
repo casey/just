74bc8d965581ddafaf455e5da78fe757ab003c61
C J Silverio <ceejceej@gmail.com>
2022-08-31 17:06:49 -0700

    Proof of concept: an include feature for justfiles. (#1)
    * Proof of concept: an include feature for justfiles.
    
    A fast proof that it's straightforward to do a no-fancy-parsing
    include feature.
    
    This design takes all lines that look like this:
    `@include "relative/path/to/file"`
    and replaces them with the contents of the pointed-to file. The resulting buffer
    is passed along to be parsed and executed as before. That's it: no attempt to
    look at the file or do anything with the contents. This is pure concatenation.
    
    * Satisfy clippy.
    
    Co-Authored-By: kevin@seaplane.io

--- src/loader.rs
+++ src/loader.rs
@@ -16,6 +16,34 @@ impl Loader {
       path: path.to_owned(),
       io_error,
     })?;
-    Ok(self.arena.alloc(src))
+
+    // Parse include directives.
+    // Include directives are lines that look like:
+    // #include "relative/path/to/file"
+    let include_regexp = Regex::new(r#"^#include "([^"]+)"$"#).unwrap();
+
+    let parent = path.parent().map_or_else(|| Path::new(""), |p| p);
+
+    let mut buf = String::new();
+    let lines = src.lines();
+    for line in lines {
+      if let Some(captures) = include_regexp.captures(line) {
+        // safe because we don't get here without a match
+        let filename = captures.get(1).unwrap().as_str();
+        let mut include_path = PathBuf::from(&parent);
+        include_path.push(filename);
+
+        let contents = fs::read_to_string(&include_path).map_err(|io_error| Error::Load {
+          path: include_path.clone(),
+          io_error,
+        })?;
+        buf.push_str(&contents);
+      } else {
+        buf.push_str(line);
+      }
+      buf.push('\n');
+    }
+
+    Ok(self.arena.alloc(buf))
   }
 }
