1ab81c5adf5dd64fda3e8e46e5af17185b7650d3
Erik Krieg <erik.c.krieg@gmail.com>
2022-09-03 01:43:16 -0400

    adds additional slash operator tests for when no lhs or rhs expression is provided

--- tests/slash_operator.rs
+++ tests/slash_operator.rs
@@ -19,11 +19,41 @@ fn twice() {
 }
 
 #[test]
-fn no_lhs() {
+fn no_lhs_once() {
   Test::new()
     .justfile("x := / 'a'")
     .args(&["--evaluate", "x"])
     .stdout("/a")
+    .run();
+}
+
+#[test]
+fn no_lhs_twice() {
+  Test::new()
+    .justfile("x := / 'a' / 'b'")
+    .args(&["--evaluate", "x"])
+    .stdout("/a/b")
+    .run();
+  Test::new()
+    .justfile("x := // 'a'")
+    .args(&["--evaluate", "x"])
+    .stdout("//a")
+    .run();
+}
+
+#[test]
+fn no_rhs_once() {
+  Test::new()
+    .justfile("x := 'a' /")
+    .stderr(
+      "
+      error: Expected backtick, identifier, '(', '/', or string, but found end of file
+        |
+      1 | x := 'a' /
+        |           ^
+    ",
+    )
+    .status(EXIT_FAILURE)
     .run();
 }
 
@@ -49,6 +79,27 @@ fn default_un_parenthesized() {
 }
 
 #[test]
+fn no_lhs_un_parenthesized() {
+  Test::new()
+    .justfile(
+      "
+      foo x=/ 'a' / 'b':
+        echo {{x}}
+    ",
+    )
+    .stderr(
+      "
+      error: Expected backtick, identifier, '(', or string, but found '/'
+        |
+      1 | foo x=/ 'a' / 'b':
+        |       ^
+    ",
+    )
+    .status(EXIT_FAILURE)
+    .run();
+}
+
+#[test]
 fn default_parenthesized() {
   Test::new()
     .justfile(
@@ -59,5 +110,19 @@ fn default_parenthesized() {
     )
     .stderr("echo a/b\n")
     .stdout("a/b\n")
+    .run();
+}
+
+#[test]
+fn no_lhs_parenthesized() {
+  Test::new()
+    .justfile(
+      "
+      foo x=(/ 'a' / 'b'):
+        echo {{x}}
+    ",
+    )
+    .stderr("echo /a/b\n")
+    .stdout("/a/b\n")
     .run();
 }
