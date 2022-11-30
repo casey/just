cd449885fd07bd3bba188c66dc0488b3533b2bd9
casey <casey@users.noreply.github.com>
2022-11-30 04:38:59 +0000

    deploy: eb924791ee494f5ead7a5306d09f6785343b3b85

--- man/zh/book.js
+++ man/zh/book.js
@@ -11,7 +11,7 @@ function playground_text(playground) {
         let editor = window.ace.edit(code_block);
         return editor.getValue();
     } else {
-        return code_block.textContent;
+        return code_block.innerText;
     }
 }
 
@@ -300,6 +300,13 @@ function playground_text(playground) {
         themePopup.querySelector("button#" + get_theme()).focus();
     }
 
+    function updateThemeSelected() {
+        themePopup.querySelectorAll('.theme-selected').forEach(function (el) {
+            el.classList.remove('theme-selected');
+        });
+        themePopup.querySelector("button#" + get_theme()).classList.add('theme-selected');
+    }
+
     function hideThemes() {
         themePopup.style.display = 'none';
         themeToggleButton.setAttribute('aria-expanded', false);
@@ -355,6 +362,7 @@ function playground_text(playground) {
 
         html.classList.remove(previousTheme);
         html.classList.add(theme);
+        updateThemeSelected();
     }
 
     // Set theme
