47d0cb37a106343ebba4ec3078be446ec98cc73d
Casey Rodarmor <casey@rodarmor.com>
2023-01-24 20:50:52 -0800

    Add test that passing dot as argument between justfiles works

--- tests/search_arguments.rs
+++ tests/search_arguments.rs
@@ -7,3 +7,23 @@ fn argument_with_different_path_prefix_is_allowed() {
     .args(["./foo", "../bar"])
     .run();
 }
+
+#[test]
+fn passing_dot_as_argument_is_allowed() {
+  Test::new()
+    .justfile(
+      "
+        say ARG:
+          echo {{ARG}}
+      ",
+    )
+    .write(
+      "child/justfile",
+      "say ARG:\n {{just_executable()}} ../say {{ARG}}",
+    )
+    .current_dir("child")
+    .args(["say", "."])
+    .stdout(".\n")
+    .stderr_regex(".*just ../say .\necho .\n")
+    .run();
+}
