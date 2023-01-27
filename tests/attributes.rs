ea3c765616797fcd6fc72d431babb87ac2329aa9
Greg Shuflin <greg@everydayimshuflin.com>
2023-01-26 23:45:15 -0800

    Specify multiple attributes on one line

--- tests/attributes.rs
+++ tests/attributes.rs
@@ -41,3 +41,65 @@ fn duplicate_attributes_are_disallowed() {
     .status(1)
     .run();
 }
+
+#[test]
+fn multiple_attributes_one_line() {
+  Test::new()
+    .justfile(
+      "
+      [macos, windows,linux]
+      [no-exit-message]
+      foo:
+        exit 1
+    ",
+    )
+    .stderr("exit 1\n")
+    .status(1)
+    .run();
+}
+
+#[test]
+fn multiple_attributes_one_line_error_message() {
+  Test::new()
+    .justfile(
+      "
+      [macos, windows linux]
+      [no-exit-message]
+      foo:
+        exit 1
+    ",
+    )
+    .stderr(
+      "
+        error: Expected ']' or ',', but found identifier
+          |
+        1 | [macos, windows linux]
+          |                 ^^^^^
+          ",
+    )
+    .status(1)
+    .run();
+}
+
+#[test]
+fn multiple_attributes_one_line_duplicate_check() {
+  Test::new()
+    .justfile(
+      "
+      [macos, windows, linux]
+      [linux]
+      foo:
+        exit 1
+    ",
+    )
+    .stderr(
+      "
+      error: Recipe attribute `linux` first used on line 1 is duplicated on line 2
+        |
+      2 | [linux]
+        |  ^^^^^
+        ",
+    )
+    .status(1)
+    .run();
+}
