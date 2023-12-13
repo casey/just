casey/just

commit 5f9ac39b038c415002c0e3e9e9b02a3e556a0883

Use `just --show` in default chooser (#1539)

src/config.rs
@@ -4,7 +4,7 @@ use {
 };
 
 // These three strings should be kept in sync:
-pub(crate) const CHOOSER_DEFAULT: &str = "fzf";
+pub(crate) const CHOOSER_DEFAULT: &str = "fzf --preview 'just --show {}'";
 pub(crate) const CHOOSER_ENVIRONMENT_KEY: &str = "JUST_CHOOSER";
 pub(crate) const CHOOSE_HELP: &str = "Select one or more recipes to run using a binary. If \
                                       `--chooser` is not passed the chooser defaults to the value \

tests/choose.rs
@@ -151,7 +151,9 @@ fn invoke_error_function() {
           echo bar
       ",
     )
-    .stderr_regex("error: Chooser `/ -cu fzf` invocation failed: .*\n")
+    .stderr_regex(
+      r"error: Chooser `/ -cu fzf --preview 'just --show \{\}'` invocation failed: .*\n",
+    )
     .status(EXIT_FAILURE)
     .shell(false)
     .args(["--shell", "/", "--choose"])
@@ -213,6 +215,7 @@ fn default() {
 
   let output = Command::new(executable_path("just"))
     .arg("--choose")
+    .arg("--chooser=fzf")
     .current_dir(tmp.path())
     .env("PATH", path)
     .output()

Cargo.toml
@@ -52,6 +52,14 @@ temptree = "0.2.0"
 which = "4.0.0"
 yaml-rust = "0.4.5"
 
+[lib]
+doctest = false
+
+[[bin]]
+path = "src/main.rs"
+name = "just"
+test = false
+
 [features]
 # No features are active by default.
 default = []

man/just.1
@@ -19,7 +19,7 @@ Run `\-\-fmt` in 'check' mode. Exits with 0 if justfile is formatted correctly. 
 .TP
 \fB\-\-choose\fR
 Select one or more recipes to run using a binary. If `\-\-chooser` is not passed the
-chooser defaults to the value of $JUST_CHOOSER, falling back to `fzf`
+chooser defaults to the value of $JUST_CHOOSER, falling back to `fzf --preview 'just --show {}'`
 .TP
 \fB\-\-clear\-shell\-args\fR
 Clear shell arguments

commit e06e12a7c0d14f85a988a221136f9cdecd15da0f

Chooser: Pass justfile path to preview command for default chooser
The default chooser is fzf, and we pass it a command to generate the
preview with "just --show" from the recipe name. This has been working
well as long as we rely on the default justfile available in the
directory, given that the preview command can find it too. However,
passing "--chose" alongside "--justfile" to select a specific file
results in the preview command not being able to find the right justfile
to process.

To address this issue, we turn the const string defining the preview
command into a function that takes the internal representation of the
justfile as an argument, and updates the preview command with the right
file.

Fixes: 5f9ac39b038c ("Use `just --show` in default chooser (#1539)")
Suggested-by: Casey Rodarmor <casey@rodarmor.com>

src/config.rs
@@ -3,13 +3,21 @@ use {
   clap::{App, AppSettings, Arg, ArgGroup, ArgMatches, ArgSettings},
 };
 
-// These three strings should be kept in sync:
-pub(crate) const CHOOSER_DEFAULT: &str =
-  "fzf --multi --preview 'just --unstable --color always --show {}'";
+// These two strings, and the two in function chooser_default below should be kept in sync:
 pub(crate) const CHOOSER_ENVIRONMENT_KEY: &str = "JUST_CHOOSER";
 pub(crate) const CHOOSE_HELP: &str = "Select one or more recipes to run using a binary. If \
                                       `--chooser` is not passed the chooser defaults to the value \
                                       of $JUST_CHOOSER, falling back to `fzf`";
+
+// Return the string for the default chooser. This is a function and not a const, because we want
+// to edit the preview command and pass it the right justfile path for the target.
+pub(crate) fn chooser_default(justfile: &Path) -> OsString {
+  let mut chooser = OsString::new();
+  chooser.push("fzf --multi --preview 'just --unstable --color always --justfile ");
+  chooser.push(justfile);
+  chooser.push(" --show {}'");
+  chooser
+}
 
 #[derive(Debug, PartialEq)]
 #[allow(clippy::struct_excessive_bools)]

src/subcommand.rs
@@ -216,7 +216,7 @@ impl Subcommand {
     let chooser = chooser
       .map(OsString::from)
       .or_else(|| env::var_os(config::CHOOSER_ENVIRONMENT_KEY))
-      .unwrap_or_else(|| OsString::from(config::CHOOSER_DEFAULT));
+      .unwrap_or_else(|| config::chooser_default(&search.justfile));
 
     let result = justfile
       .settings

tests/choose.rs
@@ -152,7 +152,7 @@ fn invoke_error_function() {
       ",
     )
     .stderr_regex(
-      r"error: Chooser `/ -cu fzf --multi --preview 'just --unstable --color always --show \{\}'` invocation failed: .*\n",
+      r"error: Chooser `/ -cu fzf --multi --preview 'just --unstable --color always --justfile [^ ]*justfile --show \{\}'` invocation failed: .*\n",
     )
     .status(EXIT_FAILURE)
     .shell(false)