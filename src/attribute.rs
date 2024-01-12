bc7636e594cbd80d7617cbb501bf57085f1733fb
Marc Beck König <mki@skytem.com>
2024-01-12 15:48:41 +0100

    add expect_args and with_arguments for Attribute enum

--- src/attribute.rs
+++ src/attribute.rs
@@ -22,7 +22,27 @@ impl Attribute {
   pub(crate) fn to_str(&self) -> &'static str {
     self.into()
   }
+
+  pub(crate) fn expect_args(&self) -> Range<usize> {
+    use Attribute::*;
+
+    match self {
+      Confirm(_) => 1..1,
+      _ => 0..0,
+    }
+  }
+
+  pub(crate) fn with_arguments(self, arguments: Vec<StringLiteral<'_>>) -> Result<Attribute, CompileErrorKind<'_>> {
+    use Attribute::*;
+
+    match self {
+      Confirm(_) if arguments.len() == 1 => Ok(Attribute::Confirm(arguments.first().map(|s| s.cooked.clone()))),
+      _ => Err(CompileErrorKind::AttributeArgumentCountMismatch { attribute: self.to_str(), found: arguments.len(), expected: self.expect_args() }),
+    }
+  }
+
 }
+
 
 #[cfg(test)]
 mod tests {
