5c259870ff017a04711060c30ae42f6095ef71d8
Casey Rodarmor <casey@rodarmor.com>
2024-05-14 18:24:25 -0700

    Remove depthawarebinding

--- src/binding.rs
+++ src/binding.rs
@@ -3,18 +3,7 @@ use super::*;
 /// A binding of `name` to `value`
 #[derive(Debug, Clone, PartialEq, Serialize)]
 pub(crate) struct Binding<'src, V = String> {
-  /// Export binding as an environment variable to child processes
-  pub(crate) export: bool,
-  /// Binding name
-  pub(crate) name: Name<'src>,
-  /// Binding value
-  pub(crate) value: V,
-}
-
-// a subclass of Binding with "depth" field
-#[derive(Debug, Clone, PartialEq, Serialize)]
-pub(crate) struct DepthAwareBinding<'src, V = String> {
-  /// Binding depth
+  /// Module depth where binding appears
   pub(crate) depth: u32,
   /// Export binding as an environment variable to child processes
   pub(crate) export: bool,
@@ -25,13 +14,6 @@ pub(crate) struct DepthAwareBinding<'src, V = String> {
 }
 
 impl<'src, V> Keyed<'src> for Binding<'src, V> {
-  fn key(&self) -> &'src str {
-    self.name.lexeme()
-  }
-}
-
-
-impl<'src, V> Keyed<'src> for DepthAwareBinding<'src, V> {
   fn key(&self) -> &'src str {
     self.name.lexeme()
   }
