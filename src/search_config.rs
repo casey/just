4989fc190b69e9eec3858b3343a0bf471f81a9ab
Casey Rodarmor <casey@rodarmor.com>
2024-05-14 22:04:26 -0700

    Enhance

--- src/search_config.rs
+++ src/search_config.rs
@@ -7,17 +7,13 @@ pub(crate) enum SearchConfig {
   /// to the root, setting the working directory to the directory in which the
   /// justfile is found.
   FromInvocationDirectory,
-
   /// As in `Invocation`, but start from `search_directory`.
   FromSearchDirectory { search_directory: PathBuf },
-
   /// Search for a justfile in a well-known global path
   Global,
-
   /// Use user-specified justfile, with the working directory set to the
   /// directory that contains it.
   WithJustfile { justfile: PathBuf },
-
   /// Use user-specified justfile and working directory.
   WithJustfileAndWorkingDirectory {
     justfile: PathBuf,
