464bd227f086594f3195162f7680c61a51d6ec33
Casey Rodarmor <casey@rodarmor.com>
2024-05-18 23:35:01 -0700

    Fix imported recipe working directory

--- src/source.rs
+++ src/source.rs
@@ -1,37 +1,41 @@
 use super::*;
 
 pub(crate) struct Source<'src> {
-  pub(crate) path: PathBuf,
-  pub(crate) depth: u32,
+  pub(crate) file_depth: u32,
   pub(crate) namepath: Namepath<'src>,
+  pub(crate) path: PathBuf,
+  pub(crate) submodule_depth: u32,
   pub(crate) working_directory: PathBuf,
 }
 
 impl<'src> Source<'src> {
   pub(crate) fn root(path: &Path) -> Self {
     Self {
-      path: path.into(),
-      depth: 0,
+      file_depth: 0,
       namepath: Namepath::default(),
+      path: path.into(),
+      submodule_depth: 0,
       working_directory: path.parent().unwrap().into(),
     }
   }
 
   pub(crate) fn import(&self, path: PathBuf) -> Self {
     Self {
-      depth: self.depth + 1,
-      path,
+      file_depth: self.file_depth + 1,
       namepath: self.namepath.clone(),
+      path,
+      submodule_depth: self.submodule_depth,
       working_directory: self.working_directory.clone(),
     }
   }
 
   pub(crate) fn module(&self, name: Name<'src>, path: PathBuf) -> Self {
     Self {
+      file_depth: self.file_depth + 1,
+      namepath: self.namepath.join(name),
+      submodule_depth: self.submodule_depth + 1,
       working_directory: path.parent().unwrap().into(),
       path,
-      depth: self.depth + 1,
-      namepath: self.namepath.join(name),
     }
   }
 }
