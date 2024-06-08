7ee2e48f2153d936fd17020a2d9e15dd8227118b
Casey Rodarmor <casey@rodarmor.com>
2024-06-08 16:32:36 +0200

    Reform

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -283,11 +283,7 @@ impl<'src, 'run> Evaluator<'src, 'run> {
     arguments: &[String],
     parameters: &[Parameter<'src>],
   ) -> RunResult<'src, (Scope<'src, 'run>, Vec<String>)> {
-    let mut evaluator = Self {
-      context: *context,
-      assignments: None,
-      scope: context.scope.child(),
-    };
+    let mut evaluator = Self::new(context, &context.scope);
 
     let mut positional = Vec::new();
 
