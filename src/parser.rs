08761839d8ee9728db02889b5738d4ffb0e26617
Casey Rodarmor <casey@rodarmor.com>
2024-09-15 16:36:43 +0700

    Use & instead of as_str

--- src/parser.rs
+++ src/parser.rs
@@ -753,7 +753,7 @@ impl<'run, 'src> Parser<'run, 'src> {
               return Err(token.error(CompileErrorKind::UnicodeEscapeEmpty));
             }
 
-            let codepoint = u32::from_str_radix(hex.as_str(), 16).unwrap();
+            let codepoint = u32::from_str_radix(&hex, 16).unwrap();
 
             cooked.push(char::from_u32(codepoint).ok_or_else(|| {
               token.error(CompileErrorKind::UnicodeEscapeRange { hex: hex.clone() })
