6fd0a7fbde3748351a5314d44b21d86ddee66841
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-23 09:58:29 -0400

    Add `xdg_*_directory` functions

--- tests/directories.rs
+++ tests/directories.rs
@@ -1,4 +1,5 @@
 use super::*;
+use etcetera::BaseStrategy;
 
 #[test]
 fn cache_directory() {
@@ -82,4 +83,112 @@ fn home_directory() {
     .args(["--evaluate", "x"])
     .stdout(dirs::home_dir().unwrap_or_default().to_string_lossy())
     .run();
+}
+
+#[test]
+fn xdg_cache_directory() {
+  Test::new()
+    .justfile("x := xdg_cache_directory()")
+    .args(["--evaluate", "x"])
+    .stdout(
+      etcetera::choose_base_strategy()
+        .unwrap()
+        .cache_dir()
+        .to_string_lossy(),
+    )
+    .run();
+}
+
+#[test]
+fn xdg_config_directory() {
+  Test::new()
+    .justfile("x := xdg_config_directory()")
+    .args(["--evaluate", "x"])
+    .stdout(
+      etcetera::choose_base_strategy()
+        .unwrap()
+        .config_dir()
+        .to_string_lossy(),
+    )
+    .run();
+}
+
+#[test]
+fn xdg_data_directory() {
+  Test::new()
+    .justfile("x := xdg_data_directory()")
+    .args(["--evaluate", "x"])
+    .stdout(
+      etcetera::choose_base_strategy()
+        .unwrap()
+        .data_dir()
+        .to_string_lossy(),
+    )
+    .run();
+}
+
+#[test]
+fn xdg_home_directory() {
+  Test::new()
+    .justfile("x := xdg_home_directory()")
+    .args(["--evaluate", "x"])
+    .stdout(
+      etcetera::choose_base_strategy()
+        .unwrap()
+        .home_dir()
+        .to_string_lossy(),
+    )
+    .run();
+}
+
+#[test]
+fn xdg_runtime_directory() {
+  if let Some(runtime_dir) = etcetera::choose_base_strategy().unwrap().runtime_dir() {
+    Test::new()
+      .justfile("x := xdg_runtime_directory()")
+      .args(["--evaluate", "x"])
+      .stdout(runtime_dir.to_string_lossy())
+      .run();
+  } else {
+    Test::new()
+      .justfile("x := xdg_runtime_directory()")
+      .args(["--evaluate", "x"])
+      .stderr(
+        "
+          error: Call to function `xdg_runtime_directory` failed: runtime directory not found
+           ——▶ justfile:1:6
+            │
+          1 │ x := xdg_runtime_directory()
+            │      ^^^^^^^^^^^^^^^^^^^^^
+        ",
+      )
+      .status(EXIT_FAILURE)
+      .run();
+  }
+}
+
+#[test]
+fn xdg_state_directory() {
+  if let Some(state_dir) = etcetera::choose_base_strategy().unwrap().state_dir() {
+    Test::new()
+      .justfile("x := xdg_state_directory()")
+      .args(["--evaluate", "x"])
+      .stdout(state_dir.to_string_lossy())
+      .run();
+  } else {
+    Test::new()
+      .justfile("x := xdg_state_directory()")
+      .args(["--evaluate", "x"])
+      .stderr(
+        "
+          error: Call to function `xdg_state_directory` failed: state directory not found
+           ——▶ justfile:1:6
+            │
+          1 │ x := xdg_state_directory()
+            │      ^^^^^^^^^^^^^^^^^^^
+        ",
+      )
+      .status(EXIT_FAILURE)
+      .run();
+  }
 }
