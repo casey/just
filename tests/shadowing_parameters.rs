8489f18fb6bda995675bb6afde0aadfb5d6c112b
Casey Rodarmor <casey@rodarmor.com>
2025-02-25 22:29:19 -0800

    Replace test macro

--- tests/shadowing_parameters.rs
+++ tests/shadowing_parameters.rs
@@ -22,10 +22,13 @@ fn shadowing_parameters_do_not_change_environment() {
     .run();
 }
 
-test! {
-  name:     exporting_shadowing_parameters_does_change_environment,
-  justfile: "export FOO := 'hello'\na $FOO:\n echo $FOO\n",
-  args:     ("a", "bar"),
-  stdout:   "bar\n",
-  stderr:   "echo $FOO\n",
+#[test]
+fn exporting_shadowing_parameters_does_change_environment() {
+  Test::new()
+    .arg("a")
+    .arg("bar")
+    .justfile("export FOO := 'hello'\na $FOO:\n echo $FOO\n")
+    .stdout("bar\n")
+    .stderr("echo $FOO\n")
+    .run();
 }
