7b410993bc42a25a686b1c39cf100db2eeba0681
Casey Rodarmor <casey@rodarmor.com>
2025-02-25 22:29:40 -0800

    Replace test macro

--- tests/functions.rs
+++ tests/functions.rs
@@ -30,18 +30,20 @@ foo:
 }
 
 #[cfg(not(windows))]
-test! {
-  name:     env_var_functions,
-  justfile: r"
+#[test]
+fn env_var_functions() {
+  Test::new()
+    .justfile(r"
 p := env_var('USER')
 b := env_var_or_default('ZADDY', 'HTAP')
 x := env_var_or_default('XYZ', 'ABC')
 
 foo:
   /usr/bin/env echo '{{p}}' '{{b}}' '{{x}}'
-",
-  stdout:   format!("{} HTAP ABC\n", env::var("USER").unwrap()).as_str(),
-  stderr:   format!("/usr/bin/env echo '{}' 'HTAP' 'ABC'\n", env::var("USER").unwrap()).as_str(),
+")
+    .stdout(format!("{} HTAP ABC\n", env::var("USER").unwrap()).as_str())
+    .stderr(format!("/usr/bin/env echo '{}' 'HTAP' 'ABC'\n", env::var("USER").unwrap()).as_str())
+    .run();
 }
 
 #[cfg(not(windows))]
