0f61fce191e0d9d6b9132642bf5ffc0ca7252e66
Rostislav Raykov <me@zbrox.com>
2025-08-15 18:24:18 +0200

    add a test for one-of-or-default

--- tests/enum.rs
+++ tests/enum.rs
@@ -15,3 +15,19 @@ fn test_one_of_parameter() {
     .stdout("b\n")
     .run();
 }
+
+#[test]
+fn test_one_of_or_default_parameter() {
+  Test::new()
+    .justfile(
+      "
+      enum foo := [ \"a\" = \"b\" ]
+
+      test-enum p1=one-of-or-default(foo, \"c\"):
+        @echo {{ p1 }}
+      ",
+    )
+    .args(["test-enum"])
+    .stdout("c\n")
+    .run();
+}
