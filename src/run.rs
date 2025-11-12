937945f6879dc0749fba2f68666a4933ad816add
Casey Rodarmor <casey@rodarmor.com>
2025-11-12 12:45:12 -0800

    Write test for signal double-initialization bug

--- src/run.rs
+++ src/run.rs
@@ -1,5 +1,10 @@
 use super::*;
 
+// options:
+// - allow passing a flag into `run` which enables or disables installing the signal handler,
+//   library users who call more than once per process should set it to `false`
+// - just don't enable the sign
+
 /// Main entry point into `just`. Parse arguments from `args` and run.
 #[allow(clippy::missing_errors_doc)]
 pub fn run(args: impl Iterator<Item = impl Into<OsString> + Clone>) -> Result<(), i32> {
@@ -33,4 +38,18 @@ pub fn run(args: impl Iterator<Item = impl Into<OsString> + Clone>) -> Result<()
       }
       error.code().unwrap_or(EXIT_FAILURE)
     })
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn run_can_be_called_more_than_once() {
+    let tmp = testing::tempdir();
+    fs::write(tmp.path().join("justfile"), "foo:").unwrap();
+    let search_directory = format!("{}/", tmp.path().to_str().unwrap());
+    run(["just", &search_directory].iter()).unwrap();
+    run(["just", &search_directory].iter()).unwrap();
+  }
 }
