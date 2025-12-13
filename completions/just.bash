8b4d28c36ac8a33c6c2d5b91abd76b047302eb7b
Joel Monroe <25249174+jtmonroe@users.noreply.github.com>
2025-12-13 10:53:06 -0500

    Global Recipe Completion with Flag
    If the preceding flag is `--global-justfile | -g`, the recipe completions will now list the global recipes. We do not need path checking since global file resolution is already implemented.

--- completions/just.bash
+++ completions/just.bash
@@ -35,13 +35,19 @@ _just() {
                     COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
                     return 0
                 else
-                    local recipes="$(just --summary 2> /dev/null) $(just -g --summary 2> /dev/null)"
+                    local recipes="$(just --summary 2> /dev/null)"
 
                     if echo "${cur}" | \grep -qF '/'; then
                         local path_prefix=$(echo "${cur}" | sed 's/[/][^/]*$/\//')
-                        local recipes="$(just --summary 2> /dev/null -- "${path_prefix}") $(just -g --summary 2> /dev/null -- "${path_prefix}")"
+                        local recipes="$(just --summary 2> /dev/null -- "${path_prefix}")"
                         local recipes=$(printf "${path_prefix}%s\t" $recipes)
                     fi
+
+                    case "${prev}" in
+                    --global-justfile | -g)
+                      local recipes=$(just -g --summary 2>/dev/null)
+                      ;;
+                    esac
 
                     if [[ $? -eq 0 ]]; then
                         COMPREPLY=( $(compgen -W "${recipes}" -- "${cur}") )
