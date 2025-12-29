d8bb70c6d66d3bfe8d605ae3f67096f58c4a7f08
Casey Rodarmor <casey@rodarmor.com>
2025-12-28 17:26:32 -0800

    Test path printing

--- tests/usage.rs
+++ tests/usage.rs
@@ -3,21 +3,23 @@ use super::*;
 #[test]
 fn usage() {
   Test::new()
-    .justfile(
+    .justfile("mod bar")
+    .write(
+      "bar.just",
       "
-        [arg('a', short='a')]
-        [arg('b', pattern='123|789', help='hello')]
-        [arg('d', short='d', long='delightful')]
-        [arg('e', short='e', pattern='abc|xyz')]
-        [arg('f', long='f', pattern='lucky')]
-        [arg('g', short='g', value='foo')]
-        foo a b c='abc' d e f='xyz' g='bar' *h:
-      ",
+[arg('a', short='a')]
+[arg('b', pattern='123|789', help='hello')]
+[arg('d', short='d', long='delightful')]
+[arg('e', short='e', pattern='abc|xyz')]
+[arg('f', long='f', pattern='lucky')]
+[arg('g', short='g', value='foo')]
+foo a b c='abc' d e f='xyz' g='bar' *h:
+",
     )
-    .args(["--usage", "foo"])
+    .args(["--usage", "bar", "foo"])
     .stdout(
       "
-        Usage: just foo [OPTIONS] b [c] [h...]
+        Usage: just bar foo [OPTIONS] b [c] [h...]
 
         Arguments:
           b hello [pattern: '123|789']
