a27f5cc007ee14f3b20b81f0467d6eb5b9b2f488
Liam <liam@scalzulli.com>
2026-02-28 12:30:07 -0500

    Remove test

--- src/compiler.rs
+++ src/compiler.rs
@@ -241,41 +241,6 @@ mod tests {
   use {super::*, temptree::temptree};
 
   #[test]
-  fn include_justfile() {
-    let justfile_a = r#"
-# A comment at the top of the file
-import "./justfile_b"
-
-#some_recipe: recipe_b
-some_recipe:
-    echo "some recipe"
-"#;
-
-    let justfile_b = r#"import "./subdir/justfile_c"
-
-recipe_b: recipe_c
-    echo "recipe b"
-"#;
-
-    let justfile_c = r#"recipe_c:
-    echo "recipe c"
-"#;
-
-    let tmp = temptree! {
-        justfile: justfile_a,
-        justfile_b: justfile_b,
-        subdir: {
-            justfile_c: justfile_c
-        }
-    };
-
-    let loader = Loader::new();
-
-    let justfile_a_path = tmp.path().join("justfile");
-    Compiler::compile(&Config::default(), &loader, &justfile_a_path).unwrap();
-  }
-
-  #[test]
   fn recursive_includes_fail() {
     let tmp = temptree! {
       justfile: "import './subdir/b'\na: b",
