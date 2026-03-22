9dad35d09cd2aa0ef6d068353e62baa5c6d5ec77
Casey Rodarmor <casey@rodarmor.com>
2026-03-21 21:46:46 -0700

    Add manual testing recipe

--- src/complete.rs
+++ src/complete.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 // todo:
+// - create a recipe to manually test completions
 // - figure out how to test these
 
 pub(crate) fn argument(current: &OsStr) -> Vec<CompletionCandidate> {
