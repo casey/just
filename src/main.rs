936f85aa58a4215ae72d9af41607ab05aec31857
Casey Rodarmor <casey@rodarmor.com>
2026-03-27 18:22:40 -0700

    Fix bash completion of command lines containing `:` (#3205)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use {
 };
 
 fn main() {
+  if env::var_os("JUST_COMPLETE").is_some_and(|value| value == "bash")
+    && env::args_os().nth(1).is_none()
+  {
+    print!("{}", include_str!("../completion-registration-script.bash"));
+    return;
+  }
+
   CompleteEnv::with_factory(Arguments::command)
     .var("JUST_COMPLETE")
     .complete();
