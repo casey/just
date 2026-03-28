09b3cbbf92aa6ad699ea9baf0fe59d5a674e8e15
Casey Rodarmor <casey@rodarmor.com>
2026-03-28 14:54:40 -0700

    Dont pass shell name to powershell (#3209)

--- tests/backticks.rs
+++ tests/backticks.rs
@@ -15,3 +15,25 @@ foos := `print('foo' * 4)`
     .stdout("foofoofoofoo")
     .success();
 }
+
+#[test]
+fn backtick_with_powershell() {
+  if !cfg!(windows) {
+    return;
+  }
+
+  Test::new()
+    .justfile(
+      r#"
+      set windows-shell := ["pwsh.exe", "-NoLogo", "-Command"]
+
+      foo := `Write-Output bar`
+
+      default:
+        @echo {{foo}}
+    "#,
+    )
+    .shell(false)
+    .stdout("bar\r\n")
+    .success();
+}
