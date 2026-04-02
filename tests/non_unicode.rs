24df7df290f4e98c396adbf864d6dd624aa91436
Casey Rodarmor <casey@rodarmor.com>
2026-04-02 15:14:00 -0700

    Reform

--- tests/non_unicode.rs
+++ tests/non_unicode.rs
@@ -1,57 +1,43 @@
 use {super::*, std::os::unix::ffi::OsStrExt};
 
+fn non_unicode_dir_name() -> &'static std::ffi::OsStr {
+  std::ffi::OsStr::from_bytes(b"foo\xff")
+}
+
 #[test]
 fn warn_for_non_unicode_invocation_directory() {
   let tempdir = tempdir();
+  let dir = tempdir.path().join(non_unicode_dir_name());
+  fs::create_dir(&dir).unwrap();
+  fs::write(dir.join("justfile"), "default:\n\ttrue\n").unwrap();
 
-  let non_unicode_dir = tempdir.path().join(std::ffi::OsStr::from_bytes(b"foo\xff"));
-
-  fs::create_dir(&non_unicode_dir).unwrap();
-  fs::write(non_unicode_dir.join("justfile"), "default:\n\ttrue\n").unwrap();
-
-  let output = Command::new(JUST)
-    .current_dir(&non_unicode_dir)
-    .arg("--color")
-    .arg("never")
-    .output()
-    .unwrap();
-
-  let stderr = str::from_utf8(&output.stderr).unwrap();
-
-  let expected = format!(
-    "The invocation directory path `{}` is not Unicode. Just is considering phasing-out support \
-    for non-Unicode paths. If you see this warning, please leave a comment on \
-    https://github.com/casey/just/issues/3229. Thank you!",
-    non_unicode_dir.display(),
-  );
-
-  assert!(stderr.contains(&expected), "stderr: {stderr}");
+  Test::with_tempdir(tempdir)
+    .no_justfile()
+    .current_dir(non_unicode_dir_name())
+    .test_round_trip(false)
+    .stderr_regex(
+      ".*The invocation directory path `[^`]+` is not Unicode\\. Just is considering phasing-out \
+      support for non-Unicode paths\\. If you see this warning, please leave a comment on \
+      https://github\\.com/casey/just/issues/3229\\. Thank you!.*",
+    )
+    .success();
 }
 
 #[test]
 fn warn_for_non_unicode_justfile_path() {
   let tempdir = tempdir();
+  let dir = tempdir.path().join(non_unicode_dir_name());
+  fs::create_dir(&dir).unwrap();
+  fs::write(dir.join("justfile"), "default:\n\ttrue\n").unwrap();
 
-  let non_unicode_dir = tempdir.path().join(std::ffi::OsStr::from_bytes(b"bar\xff"));
-
-  fs::create_dir(&non_unicode_dir).unwrap();
-  fs::write(non_unicode_dir.join("justfile"), "default:\n\ttrue\n").unwrap();
-
-  let output = Command::new(JUST)
-    .current_dir(&non_unicode_dir)
-    .arg("--color")
-    .arg("never")
-    .output()
-    .unwrap();
-
-  let stderr = str::from_utf8(&output.stderr).unwrap();
-
-  let expected = format!(
-    "The justfile path `{}` is not Unicode. Just is considering phasing-out support for \
-    non-Unicode paths. If you see this warning, please leave a comment on \
-    https://github.com/casey/just/issues/3229. Thank you!",
-    non_unicode_dir.join("justfile").display(),
-  );
-
-  assert!(stderr.contains(&expected), "stderr: {stderr}");
+  Test::with_tempdir(tempdir)
+    .no_justfile()
+    .current_dir(non_unicode_dir_name())
+    .test_round_trip(false)
+    .stderr_regex(
+      ".*The justfile path `[^`]+` is not Unicode\\. Just is considering phasing-out support for \
+      non-Unicode paths\\. If you see this warning, please leave a comment on \
+      https://github\\.com/casey/just/issues/3229\\. Thank you!.*",
+    )
+    .success();
 }
