6385ec0fe8cb83adb78b5376d66b43902d578b73
Casey Rodarmor <casey@rodarmor.com>
2026-04-07 11:20:31 -0700

    Allow comments after unexports (#3264)

--- tests/parser.rs
+++ tests/parser.rs
@@ -5,8 +5,8 @@ fn dont_run_duplicate_recipes() {
   Test::new()
     .justfile(
       "
-      set dotenv-load # foo
-      bar:
+        set dotenv-load # foo
+        bar:
       ",
     )
     .success();
@@ -17,17 +17,17 @@ fn invalid_bang_operator() {
   Test::new()
     .justfile(
       "
-      x := if '' !! '' { '' } else { '' }
+        x := if '' !! '' { '' } else { '' }
       ",
     )
     .stderr(
-      r"
-error: Expected character `=` or `~`
- ——▶ justfile:1:13
-  │
-1 │ x := if '' !! '' { '' } else { '' }
-  │             ^
-",
+      "
+        error: Expected character `=` or `~`
+         ——▶ justfile:1:13
+          │
+        1 │ x := if '' !! '' { '' } else { '' }
+          │             ^
+      ",
     )
     .failure();
 }
@@ -37,13 +37,26 @@ fn truncated_bang_operator() {
   Test::new()
     .justfile("x := if '' !")
     .stderr(
-      r"
-error: Expected character `=` or `~` but found end-of-file
- ——▶ justfile:1:13
-  │
-1 │ x := if '' !
-  │             ^
-",
+      "
+        error: Expected character `=` or `~` but found end-of-file
+         ——▶ justfile:1:13
+          │
+        1 │ x := if '' !
+          │             ^
+      ",
     )
     .failure();
+}
+
+#[test]
+fn comment_after_unexport() {
+  Test::new()
+    .justfile(
+      "
+        unexport foo # bar
+
+        baz:
+      ",
+    )
+    .success();
 }
