878c3becda4a76b24dfecae32d4dccfbd9c7a484
Casey Rodarmor <casey@rodarmor.com>
2026-05-06 00:26:50 -0700

    Enhance

--- tests/justfile_from_stdin.rs
+++ tests/justfile_from_stdin.rs
@@ -1,78 +1,15 @@
 use super::*;
 
 #[test]
-fn run() {
+fn no_working_directory() {
   Test::new()
     .no_justfile()
+    .write("bar", "baz")
     .args(["--justfile", "-"])
-    .stdin("foo:\n  echo bar\n")
-    .stderr("echo bar\n")
-    .stdout("bar\n")
+    .stdin("@foo:\n cat bar\n")
+    .stdout("baz")
     .test_round_trip(false)
     .success();
-}
-
-#[test]
-fn run_short() {
-  Test::new()
-    .no_justfile()
-    .args(["-f", "-"])
-    .stdin("foo:\n  echo bar\n")
-    .stderr("echo bar\n")
-    .stdout("bar\n")
-    .test_round_trip(false)
-    .success();
-}
-
-#[test]
-fn default_recipe() {
-  Test::new()
-    .no_justfile()
-    .args(["--justfile", "-"])
-    .stdin("foo:\n  echo bar\n\nbaz:\n  echo qux\n")
-    .stderr("echo bar\n")
-    .stdout("bar\n")
-    .test_round_trip(false)
-    .success();
-}
-
-#[test]
-fn list() {
-  Test::new()
-    .no_justfile()
-    .args(["--justfile", "-", "--list"])
-    .stdin("foo:\n  echo bar\n")
-    .stdout(
-      "
-      Available recipes:
-          foo
-    ",
-    )
-    .test_round_trip(false)
-    .success();
-}
-
-#[test]
-fn evaluate() {
-  Test::new()
-    .no_justfile()
-    .args(["--justfile", "-", "--evaluate"])
-    .stdin("x := 'foo'\n")
-    .stdout("x := \"foo\"\n")
-    .unindent_stdout(false)
-    .test_round_trip(false)
-    .success();
-}
-
-#[test]
-fn empty() {
-  Test::new()
-    .no_justfile()
-    .args(["--justfile", "-"])
-    .stdin("")
-    .stderr("error: justfile contains no recipes\n")
-    .test_round_trip(false)
-    .failure();
 }
 
 #[test]
@@ -97,45 +34,13 @@ fn init_error() {
 }
 
 #[test]
-fn init_error_with_working_directory() {
+fn with_working_directory() {
   Test::new()
     .no_justfile()
-    .args(["--justfile", "-", "--working-directory", ".", "--init"])
-    .stderr("error: cannot use justfile from standard input with `--init`\n")
-    .test_round_trip(false)
-    .failure();
-}
-
-#[test]
-fn working_directory_long() {
-  Test::new()
-    .no_justfile()
-    .tree(tree! {
-      sub: {
-        data: "qux\n",
-      },
-    })
-    .args(["--justfile", "-", "--working-directory", "sub"])
-    .stdin("foo:\n  cat data\n")
-    .stderr("cat data\n")
-    .stdout("qux\n")
-    .test_round_trip(false)
-    .success();
-}
-
-#[test]
-fn working_directory_short() {
-  Test::new()
-    .no_justfile()
-    .tree(tree! {
-      sub: {
-        data: "qux\n",
-      },
-    })
-    .args(["-f", "-", "-d", "sub"])
-    .stdin("foo:\n  cat data\n")
-    .stderr("cat data\n")
-    .stdout("qux\n")
+    .write("bar/baz", "qux")
+    .args(["--justfile", "-", "--working-directory", "bar"])
+    .stdin("@foo:\n  cat baz\n")
+    .stdout("qux")
     .test_round_trip(false)
     .success();
 }
