8c4a45e07f3e93e48c1ea35d513ae145ebe80037
Casey Rodarmor <casey@rodarmor.com>
2026-06-13 12:32:33 -0700

    Reform

--- src/justfile.rs
+++ src/justfile.rs
@@ -320,7 +320,7 @@ impl<'src> Justfile<'src> {
             if !binding.private {
               match format {
                 EvaluateFormat::Just => {
-                  println!("{0:1$} := {2}", binding.name, width, binding.value)
+                  println!("{0:1$} := {2}", binding.name, width, binding.value);
                 }
                 EvaluateFormat::Shell => {
                   if binding.export || module.settings.export {
