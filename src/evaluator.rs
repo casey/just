12381f86ada308000267a779acabc4d4b8758ad7
Casey Rodarmor <casey@rodarmor.com>
2026-06-14 13:58:00 -0700

    Make `=~` and `!~` work with lists (#3403)

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -528,18 +528,23 @@ impl<'src, 'run> Evaluator<'src, 'run> {
       ConditionalOperator::Equality => self.evaluate_value(lhs)? == self.evaluate_value(rhs)?,
       ConditionalOperator::Inequality => self.evaluate_value(lhs)? != self.evaluate_value(rhs)?,
       ConditionalOperator::RegexMatch => {
-        let lhs = self.evaluate_string(lhs)?;
+        let lhs = self.evaluate_value(lhs)?;
         let rhs = self.evaluate_string(rhs)?;
-        Regex::new(&rhs)
-          .map_err(|source| Error::RegexCompile { source })?
-          .is_match(&lhs)
+
+        let regex = Regex::new(&rhs).map_err(|source| Error::RegexCompile { source })?;
+
+        lhs.elements().iter().any(|element| regex.is_match(element))
       }
       ConditionalOperator::RegexMismatch => {
-        let lhs = self.evaluate_string(lhs)?;
+        let lhs = self.evaluate_value(lhs)?;
         let rhs = self.evaluate_string(rhs)?;
-        !Regex::new(&rhs)
-          .map_err(|source| Error::RegexCompile { source })?
-          .is_match(&lhs)
+
+        let regex = Regex::new(&rhs).map_err(|source| Error::RegexCompile { source })?;
+
+        lhs
+          .elements()
+          .iter()
+          .all(|element| !regex.is_match(element))
       }
     };
     Ok(condition)
