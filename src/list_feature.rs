5febe35e88debd4e6538468cdf465b8fa6d99b9b
Casey Rodarmor <casey@rodarmor.com>
2026-06-13 20:54:23 -0700

    Consolidate list feature errors

--- src/list_feature.rs
+++ src/list_feature.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub(crate) enum ListFeature {
   BoolFunction,
   ComparisonOperator,
@@ -11,16 +11,19 @@ pub(crate) enum ListFeature {
   ShowFunction,
 }
 
-impl ListFeature {
-  pub(crate) fn error_kind(self) -> CompileErrorKind<'static> {
+impl Display for ListFeature {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
     match self {
-      Self::BoolFunction => CompileErrorKind::BoolFunctionWithoutListsSetting,
-      Self::ComparisonOperator => CompileErrorKind::ComparisonOperatorWithoutListsSetting,
-      Self::ListLiteral => CompileErrorKind::ListLiteralWithoutListsSetting,
-      Self::LogicalOperator => CompileErrorKind::LogicalOperatorWithoutListsSetting,
-      Self::NegationOperator => CompileErrorKind::NegationOperatorWithoutListsSetting,
-      Self::NonComparisonCondition => CompileErrorKind::NonComparisonConditionWithoutListsSetting,
-      Self::ShowFunction => CompileErrorKind::ShowFunctionWithoutListsSetting,
+      Self::BoolFunction => write!(f, "the `bool()` function requires `set lists`"),
+      Self::ComparisonOperator => write!(f, "comparison operators require `set lists`"),
+      Self::ListLiteral => write!(f, "list literals require `set lists`"),
+      Self::LogicalOperator => write!(f, "logical operators require `set lists`"),
+      Self::NegationOperator => write!(f, "negation operator requires `set lists`"),
+      Self::NonComparisonCondition => write!(
+        f,
+        "`if` and `assert` conditions other than comparisons require `set lists`"
+      ),
+      Self::ShowFunction => write!(f, "the `show()` function requires `set lists`"),
     }
   }
 }
