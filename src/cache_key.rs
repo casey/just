72c7c32fc7949b372b67f08cade6f18dbd0d1422
Casey Rodarmor <casey@rodarmor.com>
2026-06-22 13:19:11 -0700

    Use body lines in cache key

--- src/cache_key.rs
+++ src/cache_key.rs
@@ -2,7 +2,7 @@ use super::*;
 
 #[derive(Serialize)]
 pub(crate) struct CacheKey<'a> {
-  pub(crate) body: String,
+  pub(crate) body: &'a [String],
   pub(crate) environment: &'a Environment,
   pub(crate) executor: &'a Executor<'a>,
   pub(crate) positional: Option<&'a [String]>,
