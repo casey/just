fdde0bd4780d4f2a09401e2db4a02ba5b30d7ab4
Casey Rodarmor <casey@rodarmor.com>
2026-06-22 18:06:57 -0700

    Amend

--- src/count.rs
+++ src/count.rs
@@ -65,11 +65,23 @@ mod tests {
 
   #[test]
   fn count() {
-    assert_eq!(Count::unnumbered("dog", 0).to_string(), "dogs");
-    assert_eq!(Count::unnumbered("dog", 1).to_string(), "dog");
-    assert_eq!(Count::unnumbered("dog", 2).to_string(), "dogs");
     assert_eq!(Count::numbered("dog", 0).to_string(), "0 dogs");
     assert_eq!(Count::numbered("dog", 1).to_string(), "1 dog");
     assert_eq!(Count::numbered("dog", 2).to_string(), "2 dogs");
+    assert_eq!(
+      Count::numbered_irregular("foot", "feet", 0).to_string(),
+      "0 feet"
+    );
+    assert_eq!(
+      Count::numbered_irregular("foot", "feet", 1).to_string(),
+      "1 foot"
+    );
+    assert_eq!(
+      Count::numbered_irregular("foot", "feet", 2).to_string(),
+      "2 feet"
+    );
+    assert_eq!(Count::unnumbered("dog", 0).to_string(), "dogs");
+    assert_eq!(Count::unnumbered("dog", 1).to_string(), "dog");
+    assert_eq!(Count::unnumbered("dog", 2).to_string(), "dogs");
   }
 }
