e1c46014096b06d7e15f03a87284094ee6b217cb
Casey Rodarmor <casey@rodarmor.com>
2026-06-25 15:38:31 -0700

    Adapt

--- src/const_eval_error.rs
+++ src/const_eval_error.rs
@@ -41,7 +41,7 @@ impl<'src> ConstEvalError<'src> {
     self.context().error(CompileErrorKind::ConstEval(self))
   }
 
-  pub(crate) fn assert_const_eval_error(error: Error<'src>) -> Self {
+  pub(crate) fn assert_const(error: Error<'src>) -> Self {
     match error {
       Error::Assert { message, name } => Self::Assert { message, name },
       Error::Const { const_error } => Self::Const(const_error),
