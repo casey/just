03db05bf7345363b5edb0deff3028594d13b0b93
Casey Rodarmor <casey@rodarmor.com>
2026-07-06 01:01:14 -0700

    Fix variable shadowing

--- tests/assignment.rs
+++ tests/assignment.rs
@@ -61,3 +61,55 @@ fn invalid_attributes_are_an_error() {
     )
     .failure();
 }
+
+#[test]
+fn local_variables_can_shadow_constants() {
+  Test::new()
+    .justfile(
+      "
+        a := HEX
+
+        HEX := 'foo'
+      ",
+    )
+    .args(["--evaluate", "a"])
+    .stdout("foo")
+    .success();
+}
+
+#[test]
+fn non_const_variables_shadowing_constants_are_an_error_in_const_contexts() {
+  Test::new()
+    .justfile(
+      "
+        HEX := `echo foo`
+
+        set tempdir := HEX
+      ",
+    )
+    .stderr(
+      "
+        error: cannot access non-const variable `HEX` in const context
+         ——▶ justfile:3:16
+          │
+        3 │ set tempdir := HEX
+          │                ^^^
+      ",
+    )
+    .failure();
+}
+
+#[test]
+fn variables_shadowing_constants_can_be_overridden() {
+  Test::new()
+    .justfile(
+      "
+        a := HEX
+
+        HEX := 'foo'
+      ",
+    )
+    .args(["--set", "HEX", "bar", "--evaluate", "a"])
+    .stdout("bar")
+    .success();
+}
