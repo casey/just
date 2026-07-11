300f8ac59fc856629f914267c149d63b7feacf7b
Casey Rodarmor <casey@rodarmor.com>
2026-07-10 18:54:00 -0700

    Blame source-later recipe for duplicate default attribute

--- tests/default.rs
+++ tests/default.rs
@@ -31,12 +31,37 @@ fn default_attribute_may_only_appear_once_per_justfile() {
     )
     .stderr(
       "
-        error: recipe `foo` has duplicate `[default]` attribute, which may only appear once per module
-         ——▶ justfile:2:1
+        error: recipe `bar` has duplicate `[default]` attribute, which may only appear once per module
+         ——▶ justfile:5:1
           │
-        2 │ foo:
+        5 │ bar:
           │ ^^^
       "
+    )
+    .failure();
+}
+
+#[test]
+fn duplicate_default_attribute_blames_source_later_recipe() {
+  Test::new()
+    .justfile(
+      "
+        [default]
+        bar:
+
+        [default]
+        foo:
+      ",
+    )
+    .arg("--summary")
+    .stderr(
+      "
+        error: recipe `foo` has duplicate `[default]` attribute, which may only appear once per module
+         ——▶ justfile:5:1
+          │
+        5 │ foo:
+          │ ^^^
+      ",
     )
     .failure();
 }
