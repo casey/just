fc4ecbb13ced5e2be57f84cf3f519004af777fa0
Casey Rodarmor <casey@rodarmor.com>
2026-07-10 22:53:55 -0700

    Tweak

--- tests/show.rs
+++ tests/show.rs
@@ -284,3 +284,21 @@ fn show_recipe_disabled_by_absent_module() {
     .stderr("error: recipe `bar` depends on absent module `foo`\n")
     .failure();
 }
+
+#[test]
+fn show_alias_disabled_by_absent_module() {
+  Test::new()
+    .justfile(
+      "
+        mod? foo
+
+        alias b := bar
+
+        bar: foo::baz
+          @echo bar
+      ",
+    )
+    .args(["--show", "b"])
+    .stderr("error: alias `b` depends on absent module `foo`\n")
+    .failure();
+}
