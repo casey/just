2ac1a44bcaee932c9de08342d41b12a64e36293a
Casey Rodarmor <casey@rodarmor.com>
2026-09-21 17:08:13 -0700

    Use snafu context selectors

--- src/datetime_format_error.rs
+++ src/datetime_format_error.rs
@@ -1,25 +1,13 @@
 use super::*;
 
-#[derive(Debug)]
+#[derive(Debug, Snafu)]
+#[snafu(visibility(pub(crate)), context(suffix(false)))]
 pub(crate) enum DatetimeFormatError {
-  Format {
-    format: String,
-  },
+  #[snafu(display("failed to format time with format string `{format}`"))]
+  Format { format: String },
+  #[snafu(display("failed to parse time format string `{format}`: {source}"))]
   Parse {
     format: String,
     source: chrono::ParseError,
   },
-}
-
-impl Display for DatetimeFormatError {
-  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-    match self {
-      Self::Format { format } => {
-        write!(f, "failed to format time with format string `{format}`")
-      }
-      Self::Parse { source, format } => {
-        write!(f, "failed to parse time format string `{format}`: {source}")
-      }
-    }
-  }
 }
